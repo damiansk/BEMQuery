b3c72c92fea7c8156b083bf0bb5d9b91ad64db6c
bumpMind <damian.s.ja@gmail.com>
2017-02-21 13:33:01 +0100

    Added test for Converter.convert()

--- tests/converter/Converter.test.js
+++ tests/converter/Converter.test.js
@@ -3,6 +3,7 @@
 
 import Converter from '../src/converter/Converter';
 import defaultConverterConfig from '../src/converter/defaultConfig';
+import Selector from '../src/converter/Selector';
 
 const expect = chai.expect;
 
@@ -24,6 +25,29 @@ describe( 'Converter', () => {
 		const converter = new Converter();
 
 		expect( converter.config ).to.deep.equal( defaultConverterConfig );
+	} );
+
+	it( 'converts BEM selectors to CSS ones', () => {
+		const selectors = {
+			'block': '.block',
+			'block elem:modifier': '.block__elem_modifier',
+			'block:modifier elem': '.block_modifier .block__elem',
+			'block:modifier elem:modifier': '.block_modifier .block__elem_modifier',
+			'block elem elem': '.block__elem__elem_modifier',
+			'block elem elem:modifier': '.block__elem__elem_modifier',
+			'block:modifier elem elem': '.block_modifier .block__elem__elem',
+			'block elem:modifier elem': '.block__elem_modifier .block__elem__elem',
+			'block:modifier elem:modifier elem': '.block_modifier .block__elem_modifier .block__elem__elem'
+		};
+		const converter = new Converter();
+
+		Object.keys( selectors ).forEach( ( selector ) => {
+			const result = converter.convert( selector );
+
+			expect( result ).to.be.an.instanceOf( Selector );
+			expect( result.BEM ).to.equal( selector );
+			expect( result.CSS ).to.equal( selectors[ selector ] );
+		} );
 	} );
 } );
 

e5fbd68d41f56f28546855ce26c7b08052c0ac6b
bumpMind <damian.s.ja@gmail.com>
2017-02-22 17:54:47 +0100

    Export classes SelectorEngine, Converter and Selector in index file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+export { default as Converter } from './converter/Converter';
+export { default as Selector } from './converter/Selector';
+export { default as SelectorEngine } from './SelectorEngine';
+export { default as factory } from './factory';

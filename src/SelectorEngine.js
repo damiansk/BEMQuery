f229dc50e52806999910ab477ce94398e03e6769
bumpMind <damian.s.ja@gmail.com>
2017-02-22 17:38:25 +0100

    Updated SelectorEngine class

--- src/SelectorEngine.js
+++ src/SelectorEngine.js
@@ -5,15 +5,33 @@ class SelectorEngine {
 		this.converter = converter;
 	}
 
-	find( selector ) {
+	find( selector, context = document ) {
 		const convertedSelector = this.converter.convert( selector );
 		const result = {
 			selector: convertedSelector
 		};
-		const elements = document.querySelectorAll( convertedSelector.CSS );
+		let cssSelector = convertedSelector.CSS;
+		let tmpId = false;
+
+		if ( context !== document ) {
+			if ( !context.id ) {
+				tmpId = true;
+				context.id = `BEMQueryTMP_${Date.now()}`
+			}
+
+			cssSelector = `#${context.id} ${cssSelector}`;
+		}
+
+		const elements = context.querySelectorAll( cssSelector );
 
 		result.elements = Array.from( elements );
+
+		if ( tmpId ) {
+			context.removeAttribute( 'id' );
+		}
 
 		return result;
 	}
 }
+
+export default SelectorEngine;
